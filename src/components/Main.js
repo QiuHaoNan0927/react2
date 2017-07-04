bbfc16f9cb777ead36f29b2727e10cd780bdc0e6
QiuHaoNan0927 <815569451@qq.com>
2017-07-04 10:38:12 +0800

    ..

--- src/components/Main.js
+++ src/components/Main.js
@@ -31,77 +31,10 @@ class AppComponent extends React.Component {
     temperature: '',
     scale: 'c'
   }
-  updatatime() {
-    this.setState({data: new Date()})
-  }
-  componentDidMount() {
-    setInterval(() => this.updatatime(), 100)
-  }
-
-  changeFlage() {
-    this.setState((prevState) => ({
-      flage: !prevState.flage
-    }));
-  }
-  changeTemperature(e) {
-    this.setState({temperature: e.target.value});
-  }
-  handleCelsiusChange(temperature) {
-    this.setState({scale: 'c', temperature});
-  }
-
-  handleFahrenheitChange(temperature) {
-    this.setState({scale: 'f', temperature});
-  }
 
   render() {
-    function toFahrenheit(celsius) {
-      return (celsius * 9 / 5) + 32;
-    }
-    function toCelsius(fahrenheit) {
-      return (fahrenheit - 32) * 5 / 9;
-    }
-    function tryConvert(temperature, convert) {
-      const input = parseFloat(temperature);
-      if (Number.isNaN(input)) {
-        return '';
-      }
-      const output = convert(input);
-      const rounded = Math.round(output * 1000) / 1000;
-      return rounded.toString();
-    }
-    const scale = this.state.scale;
-    const temperature = this.state.temperature;
-    const celsius = scale === 'f'
-      ? tryConvert(temperature, toCelsius)
-      : temperature;
-    const fahrenheit = scale === 'c'
-      ? tryConvert(temperature, toFahrenheit)
-      : temperature;
     return (
-      <div className="index">
-        <input type="text" name="name" value={this.state.temperature} onChange={:: this.changeTemperature}/>
-        <BoilingVerdict celsius={parseFloat(this.state.temperature)}></BoilingVerdict>
-        <TemperatureInput scale="c" temperature={celsius} onTemperatureChange={:: this.handleCelsiusChange}/>
-        <TemperatureInput scale="f" temperature={fahrenheit} onTemperatureChange={:: this.handleFahrenheitChange}/> {/* <button onClick={:: this.changeFlage}>flage</button>
-          <IfUserLogin flage={this.state.flage} ref="flage1"></IfUserLogin>
-          <TodoApp></TodoApp>
-          <img src={yeomanImage} alt="Yeoman Generator"/>
-          <h1 className={helloWorld}>hello world!</h1>
-          <h2>{new Date().toLocaleTimeString()}</h2>
-          <InpurToUpperCase></InpurToUpperCase>
-          <Selected></Selected>
-        <MoreInput></MoreInput> */}
-        {/* <Foo></Foo>
-          <TextInput></TextInput>
-          <StateText></StateText>
-          <InputSome></InputSome>
-          <HookFun name={titleNumber}></HookFun>
-          <GetAjax url='https://api.github.com/users/octocat/gists'></GetAjax>
-          <TimeH1></TimeH1>
-          <TimeProps data={this.state.data} text='父向子传递参数'></TimeProps>
-        <Forlist></Forlist> */}
-      </div>
+      <div className="index"></div>
     );
   }
 }
